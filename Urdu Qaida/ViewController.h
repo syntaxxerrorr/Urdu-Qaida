0fe006352ef56053e067686782dbce30d19f04a0
syntaxxerrorr <fahad@Fahads-MacBook-Pro.local>
2014-11-04 21:17:59 +0500

    Completed Urdu Alphabets Sound

--- Urdu Qaida/ViewController.h
+++ Urdu Qaida/ViewController.h
@@ -10,6 +10,50 @@
 
 @interface ViewController : UIViewController
 
+//Alphabets Button
+
+- (IBAction)firstButton:(id)sender;
+- (IBAction)alifTowButton:(id)sender;
+- (IBAction)bayButton:(id)sender;
+- (IBAction)payButton:(id)sender;
+- (IBAction)tayButton:(id)sender;
+- (IBAction)ttayButton:(id)sender;
+- (IBAction)sayButton:(id)sender;
+- (IBAction)jeemButton:(id)sender;
+- (IBAction)cheyButton:(id)sender;
+- (IBAction)heyButton:(id)sender;
+- (IBAction)kheyButton:(id)sender;
+- (IBAction)dalButton:(id)sender;
+- (IBAction)daalButton:(id)sender;
+- (IBAction)zaalButton:(id)sender;
+- (IBAction)rayButton:(id)sender;
+- (IBAction)aryButton:(id)sender;
+- (IBAction)zyButton:(id)sender;
+- (IBAction)seyButton:(id)sender;
+- (IBAction)seenButton:(id)sender;
+- (IBAction)shenButton:(id)sender;
+- (IBAction)swatButton:(id)sender;
+- (IBAction)dwatButton:(id)sender;
+- (IBAction)toyButton:(id)sender;
+- (IBAction)zoyeButton:(id)sender;
+- (IBAction)ayenButton:(id)sender;
+- (IBAction)gaeenButton:(id)sender;
+- (IBAction)fayButton:(id)sender;
+- (IBAction)qaafButton:(id)sender;
+- (IBAction)kafButton:(id)sender;
+- (IBAction)gaafButton:(id)sender;
+- (IBAction)lamButton:(id)sender;
+- (IBAction)memButton:(id)sender;
+- (IBAction)nonButton:(id)sender;
+- (IBAction)nongunButton:(id)sender;
+- (IBAction)wowButton:(id)sender;
+- (IBAction)haaButton:(id)sender;
+- (IBAction)chotiyeButton:(id)sender;
+- (IBAction)briyeButton:(id)sender;
+
+
+
+
 
 @end
 
